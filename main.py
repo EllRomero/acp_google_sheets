2d24933e3d836219a9e24aede25f3ebc5cc76581
EllRomero <ell.romero@mail.ru>
2023-12-17 15:53:56 +0300

    Added two parsing methods, for dzen and stav.aif

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
 from tg_pars import get_url
-from bf_url import get_views
+from bf_url import bf_get_view
+from sl_url import sl_get_view
 
 
 # Перечисляем подключаемые API
@@ -18,16 +19,21 @@ url = get_url(int(input('Введи кол-во постов: ')))
 # Начальная позиция для ячеек таблиц
 n = 2
 for i in url:
-    views = get_views(i)
+    # Проверка какой сайт парсить, дзен блочит request, поэтому эмулируем юзера
+    if i[8] == 's':  # stav.aif.ru
+        views = bf_get_view(i)
+    if i[8] == 'd':  # dzen.ru
+        views = sl_get_view(i)
     # Выбор ячейки C
     cell = worksheet.cell(f'{n}', '3')
-    # Установить значение в ячейку
-    cell.value = int(views)
+    # Установить значение в ячейку C
+    cell.value = views
     # Сохранение изменений
     worksheet.update_cell(cell.row, cell.col, cell.value)
-    # Запись значения в ячейку
+    # То же самое для ячейки D
     cell = worksheet.cell(f'{n}', '4')
     cell.value = i
+    # Меняем строку в таблице
     n += 1
     # Сохранение изменений
     worksheet.update_cell(cell.row, cell.col, cell.value)
